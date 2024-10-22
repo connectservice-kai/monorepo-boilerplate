5c0f8d77fa43692429cc18e76cd8205057357c32
connectservice-kai <hth5@torder.com>
2024-10-22 10:42:10 +0900

    tsconfig & eslint 설정 완료

---
+++ packages/eslint-config/react.js
@@ -0,0 +1,69 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
+import importPlugin from "eslint-plugin-import";
+
+export const reactConfig = [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+
+  {
+    // 플랫 구성에서 직접 환경 설정
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    settings: {
+      "import/resolver": {
+        typescript: true,
+        node: true,
+      },
+    },
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      // import
+      "no-unused-vars": "off",
+      "import/no-dynamic-require": "warn",
+      "import/no-nodejs-modules": "warn",
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "parent", "sibling", "index"],
+          alphabetize: { order: "desc" },
+        },
+      ],
+      "import/no-unresolved": [
+        "error",
+        {
+          ignore: ["^/"], // 절대 경로를 무시하도록 설정
+        },
+      ],
+    },
+  },
+];
--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -2,5 +2,5 @@
 export { setupCounter } from "./utils/counter";
 
 // components
-export { Header } from "./components/header";
-export { Counter } from "./components/counter";
+export { Button } from "./components/button";
+export { Input } from "./components/input";
